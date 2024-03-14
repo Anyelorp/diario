59552baf0115a3fe355d11cca2f600bf41cc0a5c
Alumno <Alumno@DESKTOP-PNMO5KJ>
2024-03-14 13:20:00 -0500

    Agregar archivos de proyecto.

---
+++ diario/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace diario
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox7 = new CheckBox();
+            label1 = new Label();
+            checkBox8 = new CheckBox();
+            checkBox9 = new CheckBox();
+            checkBox10 = new CheckBox();
+            checkBox11 = new CheckBox();
+            checkBox12 = new CheckBox();
+            checkBox13 = new CheckBox();
+            checkBox14 = new CheckBox();
+            checkBox15 = new CheckBox();
+            checkBox16 = new CheckBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(30, 119);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(92, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "DESAYUNAR";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(30, 169);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(82, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "TRABAJAR";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(30, 194);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(78, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "ESTUDIAR";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(30, 219);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(89, 19);
+            checkBox4.TabIndex = 3;
+            checkBox4.Text = "ALMORZAR";
+            checkBox4.UseVisualStyleBackColor = true;
+            checkBox4.CheckedChanged += checkBox4_CheckedChanged;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(30, 269);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(116, 19);
+            checkBox5.TabIndex = 4;
+            checkBox5.Text = "ASEO PERSONAL";
+            checkBox5.UseVisualStyleBackColor = true;
+            checkBox5.CheckedChanged += checkBox5_CheckedChanged;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(30, 144);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(77, 19);
+            checkBox6.TabIndex = 5;
+            checkBox6.Text = "BAÑARSE";
+            checkBox6.UseVisualStyleBackColor = true;
+            checkBox6.CheckedChanged += checkBox6_CheckedChanged;
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(30, 294);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(64, 19);
+            checkBox7.TabIndex = 6;
+            checkBox7.Text = "CENAR";
+            checkBox7.UseVisualStyleBackColor = true;
+            checkBox7.CheckedChanged += checkBox7_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 20.25F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.MediumBlue;
+            label1.Location = new Point(192, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(285, 32);
+            label1.TabIndex = 8;
+            label1.Text = "DIARIO SEMANAL";
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(30, 319);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(71, 19);
+            checkBox8.TabIndex = 16;
+            checkBox8.Text = "DORMIR";
+            checkBox8.UseVisualStyleBackColor = true;
+            checkBox8.CheckedChanged += checkBox8_CheckedChanged;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(30, 244);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(129, 19);
+            checkBox9.TabIndex = 17;
+            checkBox9.Text = "TIEMPO PERSONAL";
+            checkBox9.UseVisualStyleBackColor = true;
+            checkBox9.CheckedChanged += checkBox9_CheckedChanged;
+            // 
+            // checkBox10
+            // 
+            checkBox10.AutoSize = true;
+            checkBox10.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox10.Location = new Point(33, 76);
+            checkBox10.Name = "checkBox10";
+            checkBox10.Size = new Size(62, 17);
+            checkBox10.TabIndex = 18;
+            checkBox10.Text = "LUNES";
+            checkBox10.UseVisualStyleBackColor = true;
+            checkBox10.CheckedChanged += checkBox10_CheckedChanged;
+            // 
+            // checkBox11
+            // 
+            checkBox11.AutoSize = true;
+            checkBox11.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox11.Location = new Point(122, 76);
+            checkBox11.Name = "checkBox11";
+            checkBox11.Size = new Size(71, 17);
+            checkBox11.TabIndex = 19;
+            checkBox11.Text = "MARTES";
+            checkBox11.UseVisualStyleBackColor = true;
+            checkBox11.CheckedChanged += checkBox11_CheckedChanged;
+            // 
+            // checkBox12
+            // 
+            checkBox12.AutoSize = true;
+            checkBox12.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox12.Location = new Point(219, 76);
+            checkBox12.Name = "checkBox12";
+            checkBox12.Size = new Size(88, 17);
+            checkBox12.TabIndex = 20;
+            checkBox12.Text = "MIERCOLES";
+            checkBox12.UseVisualStyleBackColor = true;
+            checkBox12.CheckedChanged += checkBox12_CheckedChanged;
+            // 
+            // checkBox13
+            // 
+            checkBox13.AutoSize = true;
+            checkBox13.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox13.Location = new Point(336, 76);
+            checkBox13.Name = "checkBox13";
+            checkBox13.Size = new Size(67, 17);
+            checkBox13.TabIndex = 21;
+            checkBox13.Text = "JUEVES";
+            checkBox13.UseVisualStyleBackColor = true;
+            checkBox13.CheckedChanged += checkBox13_CheckedChanged;
+            // 
+            // checkBox14
+            // 
+            checkBox14.AutoSize = true;
+            checkBox14.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox14.Location = new Point(426, 76);
+            checkBox14.Name = "checkBox14";
+            checkBox14.Size = new Size(73, 17);
+            checkBox14.TabIndex = 22;
+            checkBox14.Text = "VIERNES";
+            checkBox14.UseVisualStyleBackColor = true;
+            checkBox14.CheckedChanged += checkBox14_CheckedChanged;
+            // 
+            // checkBox15
+            // 
+            checkBox15.AutoSize = true;
+            checkBox15.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox15.Location = new Point(524, 76);
+            checkBox15.Name = "checkBox15";
+            checkBox15.Size = new Size(70, 17);
+            checkBox15.TabIndex = 23;
+            checkBox15.Text = "SABADO";
+            checkBox15.UseVisualStyleBackColor = true;
+            checkBox15.CheckedChanged += checkBox15_CheckedChanged;
+            // 
+            // checkBox16
+            // 
+            checkBox16.AutoSize = true;
+            checkBox16.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Underline);
+            checkBox16.Location = new Point(602, 76);
+            checkBox16.Name = "checkBox16";
+            checkBox16.Size = new Size(78, 17);
+            checkBox16.TabIndex = 24;
+            checkBox16.Text = "DOMINGO";
+            checkBox16.UseVisualStyleBackColor = true;
+            checkBox16.CheckedChanged += checkBox16_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.DeepSkyBlue;
+            textBox1.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(377, 127);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 186);
+            textBox1.TabIndex = 25;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(524, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 34);
+            button1.TabIndex = 26;
+            button1.Text = "LIMPIAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(357, 342);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 34);
+            button2.TabIndex = 27;
+            button2.Text = "ACTUALIZAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Chartreuse;
+            ClientSize = new Size(687, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(checkBox16);
+            Controls.Add(checkBox15);
+            Controls.Add(checkBox14);
+            Controls.Add(checkBox13);
+            Controls.Add(checkBox12);
+            Controls.Add(checkBox11);
+            Controls.Add(checkBox10);
+            Controls.Add(checkBox9);
+            Controls.Add(checkBox8);
+            Controls.Add(label1);
+            Controls.Add(checkBox7);
+            Controls.Add(checkBox6);
+            Controls.Add(checkBox5);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            ForeColor = Color.Blue;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private Label label1;
+        private CheckBox checkBox8;
+        private CheckBox checkBox9;
+        private CheckBox checkBox10;
+        private CheckBox checkBox11;
+        private CheckBox checkBox12;
+        private CheckBox checkBox13;
+        private CheckBox checkBox14;
+        private CheckBox checkBox15;
+        private CheckBox checkBox16;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ diario/Form1.cs
@@ -0,0 +1,271 @@
+namespace diario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox2.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox2.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox3.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox3.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            if (checkBox8.Checked)
+            {
+               
+                textBox1.Text += checkBox8.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            if (checkBox1.Checked)
+            {
+                textBox1.Text += checkBox1.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void checkBox10_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            if (checkBox10.Checked)
+            {
+               
+                textBox1.Text += checkBox10.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox1.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox1.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Aquí puedes asignar el contenido que deseas mostrar en el TextBox
+            // Por ejemplo, concatenando el texto de los CheckBox seleccionados
+            textBox1.Text = "";
+
+            if (checkBox1.Checked)
+            {
+                textBox1.Text += checkBox1.Text + Environment.NewLine;
+            }
+
+            if (checkBox2.Checked)
+            {
+                textBox1.Text += checkBox2.Text + Environment.NewLine;
+            }
+
+            // Agrega los demás CheckBox de manera similar
+
+            // Si deseas incluir algún texto adicional o realizar alguna otra operación aquí, puedes hacerlo
+        }
+        private void checkBox11_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox11.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox11.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox12_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox12.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox12.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox13_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox13.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox13.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox14_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox14.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox14.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox15_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox15.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox15.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox16_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox16.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox16.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox6.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox6.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox4.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox4.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox9.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox9.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox5.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox5.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            // Verificar si el CheckBox está marcado
+            if (checkBox7.Checked)
+            {
+                // Agregar el texto del CheckBox al TextBox y agregar un salto de línea
+                textBox1.Text += checkBox7.Text + Environment.NewLine;
+            }
+            else
+            {
+                // Si deseas eliminar el texto del CheckBox cuando se desmarca, puedes hacerlo aquí
+            }
+        }
+    }
+}
